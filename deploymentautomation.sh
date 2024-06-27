92bbacd67dd66b95dd753f56f9b7e52995014213
Ubuntu <ubuntu@ip-172-31-30-163.us-east-2.compute.internal>
2024-06-27 08:51:46 +0000

    automate the deployment of an application

---
+++ deploymentautomation.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#Script to automate the deployment of an application
+
+
+# Variables
+REPO_URL="https://github.com/username/repo.git"
+DEPLOY_DIR="/var/www/app"
+BACKUP_DIR="/var/www/backup"
+LOG_FILE="/var/log/deploy.log"
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+
+# Functions
+log() {
+  echo "$TIMESTAMP - $1" >> $LOG_FILE
+}
+
+# Steps
+log "Starting deployment"
+
+# Backup current deployment
+log "Backing up current deployment"
+mkdir -p $BACKUP_DIR
+cp -r $DEPLOY_DIR $BACKUP_DIR/app_$TIMESTAMP
+
+#Fetch the latest code
+log "Fetching the latest code"
+cd $DEPLOY_DIR
+git pull $REPO_URL
+
+#Install dependencies
+log "Installing dependencies"
+npm install
+
+#Restart application
+log "Restarting application"
+systemctl restart app
+
+log "Deployment finished successfully"
+
